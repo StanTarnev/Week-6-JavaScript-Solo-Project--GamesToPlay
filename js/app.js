3852354c64fa66d2501d917041c4e3a6d049f976
Stan Tarnev <stan.tarnev@gmail.com>
2019-10-05 20:51:07 +0100

    some extensions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const deleteAllItemsButton = document.querySelector('#dlete-all');
+  const newItemForm = document.querySelector('#new-item-form');
+  newItemForm.addEventListener('submit', handleFormSubmit);
+
+  const deleteAllItemsButton = document.querySelector('#delete-all');
   deleteAllItemsButton.addEventListener('click', handleDeleteAllItems);
 });
+
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+
+  const gameList = document.querySelector('#game-list');
+
+  const gameListEntry = document.createElement('li');
+
+  const title = document.createElement('h2');
+  title.textContent = event.target.title.value;
+  const genre = document.createElement('h3');
+  genre.textContent = `Genre: ${event.target.genre.value}`;
+  const developer = document.createElement('h3');
+  developer.textContent = `Developer: ${event.target.developer.value}`;
+  const year = document.createElement('h3');
+  year.textContent = `Year: ${event.target.year.value}`;
+
+  gameListEntry.appendChild(title);
+  gameListEntry.appendChild(genre);
+  gameListEntry.appendChild(developer);
+  gameListEntry.appendChild(year);
+
+  gameList.appendChild(gameListEntry);
+
+  event.target.reset();
+};
 
 const handleDeleteAllItems = function(event) {
   const gameList = document.querySelector('#game-list');
